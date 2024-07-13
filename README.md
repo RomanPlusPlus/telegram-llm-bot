d11d4e24af2bd8921a5a8f59aaf8695e9aed53f0
RomanPlusPlus <sitelewr@gmail.com>
2024-07-13 18:06:14 +0200

    ✨feat(all): add multi-message chats, rate limiting, improve system msg and readme, refactor

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # How to deploy:
 
-1. Create a VM, and SSH to it.
+## -1. Create a telegram bot and get the token
 
-2. Clone the repository into it with "git clone"
+You can do this by talking with the official "BotFather" tool on Telegram: https://t.me/botfather
 
-3. Install the dependencies with pip
+## 0. Get the telegram IDs of the allowed users 
+
+For example, of your friends in telegram.
+
+## 1. Create a VM, and SSH to it
+
+For example, in Digital Ocean, Azure, etc.
+
+You can also run the script locally. Useful for testing.
+
+## 2. Clone the repository
+
+```
+git clone https://github.com/RomanPlusPlus/telegram-llm-bot.git
+```
+
+## 3. Create a tmux session
+
+```
+tmux new -s session_name
+```
+
+## 4. Install the dependencies with pip
 
 cd to the dir where you cloned this and run in a terminal:
 
@@ -13,25 +35,27 @@ pip install -r requirements.txt
 
 ```
 
-4. Specify the API keys like this:
+## 5. Specify the API keys and allowed users like this:
 
 ```
 echo "export TELEGRAM_LLM_BOT_TOKEN='<your_token>'" >> ~/.bashrc
 
 echo "export OPENAI_API_KEY='<your_token>'" >> ~/.bashrc
 
+echo "export OPENAI_MODEL='<your_token>'" >> ~/.bashrc
+
+echo "export ALLOWED_USER_IDS='some_id1,some_id2,some_id3'" >> ~/.bashrc
+
 source ~/.bashrc
 ```
 
-5. Run the main script in tmux
+## 5. Run the main script
 
 ```
-tmux new -s session_name
-
 python3 main.py
 ```
 
 To leave the script running even after you log out, press `Ctrl+b`, and then press `d`.
 
-To return to the session in the future, run `tmux attach -t session_name`.
+To return to the tmux session in the future, run `tmux attach -t session_name`.
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from ai_wrapper import ask_gpt_single_message
+from ai_providers.open_ai_wrapper import ask_gpt_multi_message, ask_gpt_single_message
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Application,
@@ -9,6 +9,28 @@ from telegram.ext import (
     filters,
     CallbackQueryHandler,
 )
+
+"""
+Note:
+Currently, it's limiting the number of messages from the user by keeping only the last MAX_MESSAGES_NUM messages.
+It was selected as the most user-friendly optiion, even if it's costlier.
+Some possible alternative strategies:
+- maybe detect a change of topic and reset the chat
+- add the reset chat button
+- reset after a night
+--- save the ts of the latest msg
+--- if the latest msg by the user was yesterday, and more than 5h elapsed, then reset
+
+############################################################
+
+TODO:
+-implement Claude 3.5 Sonet (smarter, larger context window)
+
+photo as an input
+
+add optional logging, by user
+
+"""
 
 
 # Retrieve token from environment variable
@@ -20,8 +42,22 @@ if not TOKEN:
 
 
 allowed_ids_str = os.getenv("ALLOWED_USER_IDS")
+
 # Convert the string of comma-separated integers to a list of integers
 ALLOWED_USER_IDS = [int(user_id.strip()) for user_id in allowed_ids_str.split(",")]
+
+SYSTEM_MSG = """
+Du bist ein hilfreicher Assistent.
+Dies ist ein Instant-Messaging-Chat, also halten Sie Ihre Antworten kurz und präzise. Geben Sie aber eine ausführliche Antwort, wenn Sie dem Nutzer damit am besten helfen können. 
+Zum Beispiel, wenn der Benutzer eine einfache Frage gestellt hat, ist eine kurze Antwort vorzuziehen. Wenn der Benutzer jedoch eine komplizierte E-Mail schreiben möchte, schreiben Sie sie vollständig. 
+Oft ist es hilfreich, dem Benutzer Fragen zu stellen, um ihm einen maßgeschneiderten Rat zu geben oder das Thema zu vertiefen. 
+Aber der Benutzer tippt nicht gerne, also versuchen Sie, unnötige Fragen zu vermeiden. Und es ist besser, eine Frage zu stellen, NACHDEM Sie dem Nutzer bereits geholfen haben, als eine Option zum Weitermachen. 
+Verwenden Sie die Sprache des Benutzers, es sei denn, eine Aufgabe erfordert etwas anderes.
+"""
+
+MAX_MESSAGES_NUM = 50
+
+MESSAGES_BY_USER = {}
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -30,9 +66,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
 
     if True:
-        keyboard = [
-            [InlineKeyboardButton("Start", callback_data="start_game")]
-        ]
+        keyboard = [[InlineKeyboardButton("Start", callback_data="start_game")]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         welcome_message = f"Hello {user.first_name}, ich bin dein hilfreicher Assistent! Clicke 'Start'"
         await context.bot.send_message(
@@ -59,23 +93,47 @@ async def start_game_callback(
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    text = update.message.text
-
     user_id = update.effective_user.id
 
     if user_id in ALLOWED_USER_IDS:
-        # await update.message.reply_text(f"Du hast gesendet: {text}")
-        sys_msg = "Du bist ein hilfreicher Assistent."
-        answer = ask_gpt_single_message(text, sys_msg, max_length=500)
+        user_input = update.message.text
+
+        if user_id in MESSAGES_BY_USER:
+            MESSAGES_BY_USER[user_id].append(
+                {"role": "user", "content": user_input},
+            )
+
+        else:  # the user posted his first message
+            MESSAGES_BY_USER[user_id] = [
+                {"role": "system", "content": SYSTEM_MSG},
+                {"role": "user", "content": user_input},
+            ]
+
+        # answer = ask_gpt_single_message(user_input, SYSTEM_MSG, max_length=500)
+        answer = ask_gpt_multi_message(MESSAGES_BY_USER[user_id], max_length=500)
+
+        MESSAGES_BY_USER[user_id].append(
+            {"role": "assistant", "content": answer},
+        )
+
+        # remove the oldest messages. We keep only the last MAX_MESSAGES_NUM messages
+        if len(MESSAGES_BY_USER[user_id]) > MAX_MESSAGES_NUM:
+            MESSAGES_BY_USER[user_id] = MESSAGES_BY_USER[user_id][-MAX_MESSAGES_NUM:]
+            # attach the system message to the beginning
+            MESSAGES_BY_USER[user_id].insert(0, {"role": "system", "content": SYSTEM_MSG})
+        print(f"Messages length: {len(MESSAGES_BY_USER[user_id])}")
+
         await update.message.reply_text(answer)
     else:
-        text = f"Eh? Du hast doch keine Berechtigung. Deine user_id ist {user_id}."
-        await update.message.reply_text(text)
+        answer = f"Eh? Du hast doch keine Berechtigung. Deine user_id ist {user_id}."
+        print(answer)
+        await update.message.reply_text(answer)
 
 
 async def restrict(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
-
+    text = f"Keine Berechtigung für user_id {user_id}."
+    print(text)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"Keine Berechtigung für user_id {user_id}.",
